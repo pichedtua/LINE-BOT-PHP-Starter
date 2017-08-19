33b41d88ce4c5115d655aa71cfe9880e53b53a5a
pichedtua <piched_pu@hotmail.com>
2017-08-19 16:24:49 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,3 @@
+curl -X GET \
+-H 'Authorization: Bearer {RsPR7b8G45RVH5r+NWu/fUcY5YRJziuVmwCG2z712gsiFV82UrXXgSCyBcXlXIONTUDYyzL+47syO7KHSgrt0/uzLpkkQaCqjuzrOUEA5wxkwKz0ne29RjGEp7ORs2ukUmgclKEof4T8iIsoF+dqlAdB04t89/1O/w1cDnyilFU=}' \
+https://api.line.me/v1/oauth/verify
